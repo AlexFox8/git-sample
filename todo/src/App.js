1273961b755a2d5b4f9b3067e7c7ef6525a6fa08
Alex Lutsiuk <oleksandr.lutsiuk@gmail.com>
2020-09-05 01:41:59 +0300

    start project

---
+++ todo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <h1> Todo app</h1>
+    </div>
+  );
+}
+
+export default App;
