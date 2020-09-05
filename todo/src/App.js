eb36a35aa663be0ee5f2639fdc31645c9d1b79bc
Alex Lutsiuk <oleksandr.lutsiuk@gmail.com>
2020-09-05 17:00:38 +0300

    add todos&icons

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="todo-app">
-      <TodoForm/>
+      <TodoList/>
     </div>
   );
 }
--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,7 +1,6 @@
 import React, {useState} from "react";
-
 function TodoForm (props) {
-    const [input, setInput] = useState('here')
+    const [input, setInput] = useState('')
 
     const handleChange = e => {
     setInput(e.target.value)
@@ -14,6 +13,8 @@ function TodoForm (props) {
             id: Math.floor(Math.random() * 10000),
             text: input
         });
+
+        setInput('');
     }
 
     return (
