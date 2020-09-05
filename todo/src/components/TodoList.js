6afe553457f6c87438da73a74bb9a1b849d0e355
Alex Lutsiuk <oleksandr.lutsiuk@gmail.com>
2020-09-06 02:11:38 +0300

    fix bugs & add style

--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -13,11 +13,22 @@ function TodoList(){
         setTodos(newTodos);
     };
 
+    const updateTodo = (todoId, newValue) => {
+        if(!newValue.text || /^\s*$/.test(newValue.text)){
+            return
+        }
+
+        setTodos(prev => prev.map(item =>(item.id === todoId ? newValue : item))
+        );
+    }
+
         const removeTodo = id => {
             const removeArr = [...todos].filter(todo => todo.id !== id)
 
             setTodos(removeArr);
-        }
+        };
+
+
 
         const completeTodo = id => {
           let updatedTodos = todos.map(todo => {
@@ -32,7 +43,7 @@ function TodoList(){
         <div>
             <h1>What's the plan for Today ?</h1>
             <TodoForm onSubmit={addTodo} />
-            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
+            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo} />
         </div>
     )
 }
